763793645ad0107ab1f4ecf386c5d92e02a04705
ballas888 <ritchierunner@gmail.com>
2015-05-03 19:56:08 +1200

    Started to create a framework

--- AzulProject/www/js/gameObject.js
+++ AzulProject/www/js/gameObject.js
@@ -7,8 +7,12 @@ gameObject.prototype.draw = function(context){
   context.fillRect(this.x,this.y, 20,20);  
 };
 
-gameObject.prototype.update = function(context){
-    
-    this.y += 1;
+gameObject.prototype.update = function(context){ 
+    var canvas = context.canvas
+    var canWidth = canvas.width;
+    var canHeight = canvas.height;
+    if((this.y+1)+20<canHeight){
+       this.y += 1; 
+    }    
 };
 
