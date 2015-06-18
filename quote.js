b487852d652fbbbb82687fe0ec64e43483676c73
ballas888 <ritchierunner@gmail.com>
2015-06-18 13:37:48 +1200

    some old things conflicting with new fixed now

--- quote.js
+++ quote.js
@@ -3,10 +3,15 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 var app = express();
 var pg = require('pg');
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
+var connectionString = process.env.DATABASE_URL;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname));
 app.use(cors());
+
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
