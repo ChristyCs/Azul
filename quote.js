51833fae6fc9b38a0dcb872eeb6441881cfd31cd
ballas888 <ritchierunner@gmail.com>
2015-06-19 00:01:38 +1200

    Made ahole new login page and made code way cleaner

--- quote.js
+++ quote.js
@@ -9,7 +9,7 @@ var connectionString = process.env.DATABASE_URL;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname));
+app.use(express.static(__dirname+"/public"));
 app.use(cors());
 
 app.post('/add', function (request, response) {
