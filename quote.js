4e523e618d8efc1a3fc4b9b4a5640a08da28bf9a
ballas888 <ritchierunner@gmail.com>
2015-06-18 13:24:01 +1200

    Its all fixed now. thank god

--- quote.js
+++ quote.js
@@ -1,2 +1,12 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var app = express();
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
 
-
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname));
+app.use(cors());
