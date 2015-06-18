6f7a0faba9fd539d0806b7a964aeea313c5c5054
ballas888 <ritchierunner@gmail.com>
2015-06-18 13:28:39 +1200

    some old things conflicting with new fixed now

--- quote.js
+++ quote.js
@@ -5,7 +5,7 @@ var app = express();
 var pg = require('pg');
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname));
