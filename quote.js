e5c5cdfef4f401802f6f887e0e7580507f050d7f
ballas888 <ritchierunner@gmail.com>
2015-06-18 16:10:36 +1200

    Update quote.js

--- quote.js
+++ quote.js
@@ -57,8 +57,8 @@ app.post('/login', function(request, response){
                             response.statusCode = 500;
                             response.send(error);
                         }else{
-                            response.send(result);
-                            password(result.row[0].password).verifyAgainst(hash, function(error, verified){
+                            console.log(result.rows[0].password);
+                            password(result.rows[0].password).verifyAgainst(hash, function(error, verified){
                                 if(error){
                                     response.statusCode = 500;
                                     response.send(error);
