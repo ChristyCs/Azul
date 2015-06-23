d330daf1a1a422bf8b45e8bc59c7e9625ea2a829
ballas888 <ritchierunner@gmail.com>
2015-06-23 23:34:30 +1200

    Added editor for maps. Still have a lot to do. hopefully finish on Wednesday

--- AzulProject/platforms/android/assets/www/cordova_plugins.js
+++ AzulProject/platforms/android/assets/www/cordova_plugins.js
@@ -1,7 +1,18 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-vibration/www/vibration.js",
+        "id": "cordova-plugin-vibration.notification",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "cordova-plugin-vibration": "1.2.1-dev"
+}
 // BOTTOM OF METADATA
 });
